d38358d9173e3726bf2eb9a8dc2f0d599feee24a
Jean-Baptiste Musso <jeanbaptiste@tcmlabs.fr>
2020-10-18 14:14:37 +0200

    Add bin/create-token.sh helper script

---
+++ bin/create-token.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+curl -L -X POST 'http://localhost:8081/v1/auth' \
+  -H 'Content-Type: application/json' \
+  --data-raw '{
+    "username": "cassandra",
+    "password": "cassandra"
+}'
